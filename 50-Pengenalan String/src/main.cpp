bf25b2781fba24b4cbe8e883c6d0467be206d121
maulanawesome5 <60890352+Maulanawesome5@users.noreply.github.com>
2021-10-14 20:27:13 +0700

    Saya upload semua karena malas

---
+++ 50-Pengenalan String/src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string kata("mobil");
+    cout << kata << endl;
+
+    string data;
+    cout << "Masukkan kata/kalimat : ";
+    cin >> data;
+    cout << "kata/kalimat yang dimasukkan = ";
+    cout << data << endl;
+
+    return 0;
+}
