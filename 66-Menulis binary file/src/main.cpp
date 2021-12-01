0bc9e7c45eff2f534b3e168f4bcf450c987c65bc
Maulanawesome5 <maulanawesome5@gmail.com>
2021-12-02 02:17:07 +0700

    Melanjutkan series latihan standard library fstream, dan merevisi beberapa bagian

--- 66-Menulis binary file/src/main.cpp
+++ 66-Menulis binary file/src/main.cpp
@@ -7,10 +7,12 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     fstream myFile;
-    int angka = 3500;
-
-    myFile.open("data.bin", ios::out | ios::binary);
-    // myFile << angka;
+    int angka = 12345;
+    
+    /* data.bin tidak bisa dibaca oleh Windows */
+    // myFile.open("data.bin", ios::out | ios::binary);
+    
+    myFile.open("data.txt", ios::out | ios::binary);
     myFile.write(reinterpret_cast<char*>(&angka), sizeof(angka));
     myFile.close();
 
