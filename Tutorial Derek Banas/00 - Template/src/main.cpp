a35dd6290c4db64e7e256c77fdb9271deaddcd90
Maulanawesome5 <maulanawesome5@gmail.com>
2022-06-14 01:22:24 +0700

    DerekBanas: Advance C++ tutorial

---
+++ Tutorial Derek Banas/00 - Template/src/main.cpp
@@ -0,0 +1,22 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <limits>
+#include <vector>
+#include <sstream>
+#include <numeric>
+#include <ctime>
+#include <cmath>
+
+using namespace std;
+
+int imGlobal = 0;
+const double PI = 3.14;
+
+int main(int argc, char const *argv[])
+{
+    cout << "testing" << endl;
+    cout << "build sukses" << endl;
+
+    return 0;
+}
